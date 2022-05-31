94b5fdcce3b095d1663faae9024d7ed0e2f0b29a
andresacevedog <andres.acevedo@jdl-solutions.co>
2022-05-31 10:42:59 -0500

    Aplico cambio de temas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createBrowserHistory } from 'history';
+import { ThemeProvider } from 'styled-components';
 import { Router } from 'react-router-dom';
 import routes, { renderRoutes } from 'routes';
 import {
@@ -7,7 +8,9 @@ import {
 } from 'react-bootstrap';
 import { Moon as MoonIcon } from 'react-feather';
 import { masterMessages } from 'constantes';
+import { lightTheme, darkTheme } from './theme';
 import AppStyles from './App.css';
+import GlobalStyles from './global';
 
 const { title } = masterMessages.app; // Carga los mensajes a mostrar
 
@@ -15,24 +18,35 @@ const { title } = masterMessages.app; // Carga los mensajes a mostrar
 const history = createBrowserHistory();
 
 function App() {
+  const [theme, setTheme] = useState(lightTheme); // tema predeterminado
+
   return (
-    <Router history={history}>
-      <div className={AppStyles.App}>
-        {/* Header */}
-        <Navbar expand="lg" variant="light" bg="light">
-          <Container>
-            <Navbar.Brand href="/">{ title }</Navbar.Brand>
-            <Nav>
-              <MoonIcon size={22} style={{ marginTop: 4 }} />
-              <Button size="sm" style={{ fontSize: 11, margin: 2 }}>
-                Light / Dark
-              </Button>
-            </Nav>
-          </Container>
-        </Navbar>
-        {renderRoutes(routes)}
-      </div>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Router history={history}>
+          <div className={AppStyles.App}>
+            {/* Header */}
+            <Navbar expand="lg" variant="light" bg="light">
+              <Container>
+                <Navbar.Brand href="/">{ title }</Navbar.Brand>
+                <Nav>
+                  <MoonIcon size={22} style={{ marginTop: 4 }} />
+                  <Button
+                    size="sm"
+                    style={{ fontSize: 11, margin: 2 }}
+                    onClick={() => setTheme((theme === lightTheme) ? darkTheme : lightTheme)}
+                  >
+                    Light / Dark
+                  </Button>
+                </Nav>
+              </Container>
+            </Navbar>
+            {renderRoutes(routes)}
+          </div>
+        </Router>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/global.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
+    transition: all 0.25s linear;
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+ }
+`;
+
+export default GlobalStyles;
